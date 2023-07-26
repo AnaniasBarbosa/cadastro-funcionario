dd2d53a53849ceb791ef020a6a9881ab797a6c78
Ananias Barbosa <ananiasgomes27@gmail.com>
2023-07-26 11:38:52 -0300

    adicionado metodos no modal de apresentacao

--- util/cadastraFuncionario.js
+++ util/cadastraFuncionario.js
@@ -5,11 +5,17 @@ export default function cadastraFuncionario(nome, idade, cargo, extra) {
   const campoExtra = cargo == "gerente" ? "DEPARTAMENTO" : "LINGUAGEM"
   const funcionario =
     cargo == "gerente" ? new Gerente(nome, idade, cargo, extra) : new Desenvolvedor(nome, idade, cargo, extra)
+  const apresentacao = funcionario.seApresentar()
+  const funcao = cargo == "gerente" ? funcionario.gerenciar() : funcionario?.programar()
+  const trabalhando = funcionario.trabalhar()
 
-  const modalNome = (document.querySelector('[id="nome-cadastro"]').textContent = `NOME: ${funcionario.nome}`)
-  const modalIdade = (document.querySelector('[id="idade-cadastro"]').textContent = `IDADE: ${funcionario.idade}`)
-  const modalCargo = (document.querySelector('[id="cargo-cadastro"]').textContent = `CARGO: ${funcionario.cargo}`)
-  const modalCampoExtra = (document.querySelector('[id="extra-cadastro"]').textContent = `${campoExtra}: ${
+  document.querySelector('[id="nome-cadastro"]').textContent = `NOME: ${funcionario.nome}`
+  document.querySelector('[id="idade-cadastro"]').textContent = `IDADE: ${funcionario.idade}`
+  document.querySelector('[id="cargo-cadastro"]').textContent = `CARGO: ${funcionario.cargo}`
+  document.querySelector('[id="apresentacao"]').textContent = apresentacao
+  document.querySelector('[id="trabalhando"]').textContent = trabalhando
+  document.querySelector('[id="funcao"]').textContent = funcao
+  document.querySelector('[id="extra-cadastro"]').textContent = `${campoExtra}: ${
     funcionario.departamento || funcionario.linguagem
-  }`)
+  }`
 }
